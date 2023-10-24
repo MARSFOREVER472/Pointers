ac829d3c0527e9748d58107fba1ffad1947ba0fd
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-24 16:57:52 -0300

    Variables agregados a los punteros.

--- Punteros/Punteros.cpp
+++ Punteros/Punteros.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string> // IMPORTANTE!!!!!!!
+
+using namespace std; // IMPORTANTE!!!!!!!
 
 int main()
 {
-    // EN INSTANTES...
+    // Vamos a crear un puntero asociado a una variable entera, en cadena y en booleanos.
+
+    int* example_pointer; // Para una variable en "integer".
+    std::string* string_pointer; // Para una variable en "string".
+    bool* boolean_pointer;// Para una variable "booleana".
 }
 
