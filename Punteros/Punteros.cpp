7b8c714e5eac3105b5e7128d4b2f122f60964dd0
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-25 10:07:44 -0300

    Se desplegaron los resultados pedidos por consola.

--- Punteros/Punteros.cpp
+++ Punteros/Punteros.cpp
@@ -5,10 +5,17 @@ using namespace std; // IMPORTANTE!!!!!!!
 
 int main()
 {
+    // COMENTAREMOS TODO POR EL MOMENTO...
     // Vamos a crear un puntero asociado a una variable entera, en cadena y en booleanos.
 
-    int* example_pointer; // Para una variable en "integer".
-    std::string* string_pointer; // Para una variable en "string".
-    bool* boolean_pointer;// Para una variable "booleana".
+    // int* example_pointer; // Para una variable en "integer".
+    // std::string* string_pointer; // Para una variable en "string".
+    // bool* boolean_pointer;// Para una variable "booleana".
+
+    // VAMOS A DESPLEGAR UN VALOR NUMÉRICO MEDIANTE PUNTEROS:
+
+    int number = 3; // VARIABLE NUMÉRICA.
+    cout << number << endl;
+    cout << &number; // DESPLIEGA ENTREGANDO TODO EL CONTENIDO DE DIRECCIÓN DE MEMORIA QUE APUNTA EN UNA VARIABLE.
 }
 
