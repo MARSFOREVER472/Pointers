20ccc9fe8e2f241b72b4c95304ed6b2927520ac6
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-25 22:26:18 -0300

    Ingresando direcciones de memoria de prueba.

--- Punteros/Punteros.cpp
+++ Punteros/Punteros.cpp
@@ -17,6 +17,7 @@ int main()
     int number = 3; // VARIABLE NUMÉRICA.
     int* pointer_number; // VARIABLE DEL PUNTERO DE UN VALOR NUMÉRICO.
     pointer_number = &number; // APUNTA A UN NÚMERO DESPLEGADO POR CONSOLA MEDIANTE UN TROZO DE DIRECCIÓN DE MEMORIA.
+
     cout << pointer_number << endl; // DESPLIEGA EL VALOR MENCIONADO ANTERIORMENTE JUNTO CON EL NÚMERO INGRESADO.
     cout << &number << endl; // DESPLIEGA ENTREGANDO TODO EL CONTENIDO DE DIRECCIÓN DE MEMORIA QUE APUNTA EN UNA VARIABLE.
     cout << *pointer_number << endl; // IMPRIME EL VALOR 3 HACIENDO SALTO DE LÍNEA.
@@ -24,6 +25,8 @@ int main()
     // AHORA SE DESPLEGARÁ UN VALOR ENTERO NUMÉRICO...
 
     *pointer_number = 4; // VALOR NUMÉRICO.
+
+    cout << pointer_number << endl; // LA DIRECCIÓN DE MEMORIA VA A SEGUIR EXISTIENDO AL DESPLEGAR EN LA CONSOLA.
 
     cout << *pointer_number; // IMPRIME EL VALOR NUMÉRICO (4).
 }
